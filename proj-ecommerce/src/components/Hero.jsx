1c597c89d4917be73bd7ae8fadfbd259356572d5
augusto <augusdpmenezes@gmail.com>
2023-04-18 10:31:16 -0300

    save

--- proj-ecommerce/src/components/Hero.jsx
+++ proj-ecommerce/src/components/Hero.jsx
@@ -1,30 +1,66 @@
 import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const Hero = () => {
+
+  const setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  }
+
   return (
     <>
-      <section className='text-white bg-no-repeat bg-cover bg-center py-24 bg-purple-400 h-[800px] font-montserrat' style={{ backgroundImage: "url('https://i.postimg.cc/FKp9b1yZ/AUTUMN-1.png')" }} >
-        <div className='w-5/6 mx-auto flex h-full justify-around'>
-          <div className='flex h-full justify-center items-center sm:items-baseline'>
-            <div className='h-full flex justify-center  flex-col'>
-              <div className='items-center flex flex-row uppercase'>
-                <div className='w-10 h-[2px] bg-purple-900 mr-2' />Novos Lançamentos
-              </div>
-              <div className='flex flex-col'>
-                <div className='text-7xl leading-none mb-4 flex flex-col uppercase' >
-                  <span className='font-oswald'>Vendas de primavera</span> 
-                  <span className='text-xs py-2' >Para</span>
-                  <span className='font-bold font-oswald' >Mulheres</span>
+      <Slider {...setting} className='overflow-hidden h-screen' >
+      <section className='text-white bg-no-repeat bg-cover bg-center pt-24 bg-purple-400 h-screen font-montserrat' >
+          <div className='w-5/6 mx-auto flex h-full justify-around'>
+            <div className='flex h-full justify-center items-center sm:items-baseline'>
+              <div className='h-full flex justify-center  flex-col'>
+                <div className='items-center flex flex-row uppercase'>
+                  <div className='w-10 h-[2px] bg-purple-900 mr-2' />Novos Lançamentos
+                </div>
+                <div className='flex flex-col'>
+                  <div className='text-7xl leading-none mb-4 flex flex-col uppercase' >
+                    <span className='font-oswald'>Vendas de primavera</span>
+                    <span className='text-xs py-2' >Para</span>
+                    <span className='font-bold font-oswald' >Mulheres</span>
+                  </div>
                 </div>
               </div>
             </div>
-
+            <div className='hidden lg:block'>
+              <img className='h-full' src="https://i.postimg.cc/0NMfDJ1g/7027592h1l4b8tartgo3jh0gme-74302643e0aedeb75985c7a425d72610.png" alt="Woman" />
+            </div>
           </div>
-          <div className='hidden lg:block'>
-            <img className='h-[705px]' src="https://i.postimg.cc/0NMfDJ1g/7027592h1l4b8tartgo3jh0gme-74302643e0aedeb75985c7a425d72610.png" alt="Woman" />
+        </section>
+        <section className='text-white bg-no-repeat bg-cover bg-center pt-24 bg-purple-400 h-screen font-montserrat' >
+          <div className='w-5/6 mx-auto flex flex-row-reverse h-full justify-around'>
+            <div className='flex h-full justify-center items-center sm:items-baseline'>
+              <div className='h-full flex justify-center flex-col'>
+                <div className='items-center flex flex-row uppercase'>
+                  <div className='w-10 h-[2px] bg-purple-900 mr-2' />NOVIDADES
+                </div>
+                <div className='flex flex-col'>
+                  <div className='text-7xl leading-none mb-4 flex flex-col uppercase' >
+                    <span className='font-oswald'>Todos os dias</span>
+                    <span className='text-xs py-2' >Várias novas</span>
+                    <span className='font-bold font-oswald' >Ofertas</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='hidden lg:block h-full pt-40'>
+              <img className='h-full' src="https://i.postimg.cc/yN8CSxMH/t7cu6kqlnf2dmna4u3ubqdhijd-ea1bfb6d20238060242904fa450117d1.png" alt="Woman" />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </Slider>
     </>
   )
 }
